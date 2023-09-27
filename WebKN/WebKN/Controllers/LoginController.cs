37e031e41a7ae6153743ec3d2ad46d98c4cb6801
Johel Martinez <johelamb27@gmail.com>
2023-09-26 18:28:11 -0600

    Commit 0: Semana 03 Programacion Avanzada Martes Noche

---
+++ WebKN/WebKN/Controllers/LoginController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebKN.Entities;
+using WebKN.Models;
+
+namespace WebKN.Controllers
+{
+    public class LoginController : Controller
+    {
+        UsuarioModel claseUsuario = new UsuarioModel();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpGet]
+        public ActionResult IniciarSesion()
+        { 
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult IniciarSesion(UsuarioEnt entidad)
+        {
+            claseUsuario.IniciarSesion(entidad);
+            return View();
+        }
+
+
+        [HttpGet]
+        public ActionResult RegistrarCuenta()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult RegistrarCuenta(UsuarioEnt entidad)
+        {
+            claseUsuario.RegistrarCuenta(entidad);
+            return View();
+        }
+
+
+        [HttpGet]
+        public ActionResult RecuperarCuenta()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult RecuperarCuenta(UsuarioEnt entidad)
+        {
+            claseUsuario.RecuperarCuenta(entidad);
+            return View();
+        }
+
+    }
+}
---
+++ WebKN/WebKN/Models/Usuario.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebKN.Models
+{
+    public class Usuario
+    {
+        public string MostrarNombre()
+        {
+            string nombre = "Thompson";
+
+            return nombre;
+        }
+    }
+}
